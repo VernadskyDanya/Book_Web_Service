6101cb7836134dc7b67f828abb4798d9f02ea0dc
Daniil <d.vernadskii@gmail.com>
2023-10-27 03:00:36 +0300

    feat: updated service version

--- src/app/settings/app.py
+++ src/app/settings/app.py
@@ -5,5 +5,5 @@ class AppConfig:
     HOST = env.str("HOST_ENV")
     PORT = env.int("PORT_ENV")
     CLIENT_MAX_SIZE = env.int("CLIENT_MAX_SIZE", default=1024 * 1024 * 10)
-    VERSION = "0.1.0"
+    VERSION = "0.1.2"
     DEBUG = env.bool("DEBUG", default=False)
